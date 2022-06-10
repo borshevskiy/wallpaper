409860b43793eba38dbc6b1afc22e414f4b93cc3
SergeBorshevskiy <sk8er1001@mail.ru>
2022-06-10 11:07:43 +0600

    95% ready

--- app/src/main/java/com/template/MainActivity.kt
+++ app/src/main/java/com/template/MainActivity.kt
@@ -1,50 +1,27 @@
 package com.template
 
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.template.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(), View.OnTouchListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navController = findNavController(R.id.nav_host_fragment)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-    }
-
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-        if (event!!.pointerCount == 2) {
-            Toast.makeText(this, "After 1 second you will exit the application", Toast.LENGTH_SHORT).show()
-            CoroutineScope(Dispatchers.Main).launch {
-                delay(1000)
-                finish()
-            }
-        }
-        return true
-    }
 }
